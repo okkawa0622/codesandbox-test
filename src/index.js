90d6ab0bdc72a7442f84b8c77aa89455250943c3
okukawaR <54783426+okkawa0622@users.noreply.github.com>
2022-04-12 15:10:21 +0900

    変数について

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import "./styles.css";
+/**
+ * varは再宣言が可能
+ * letは再宣言が不可能
+ * constは再宣言も上書きも不可能
+ * ※ただしconstで定義したオブジェクトはプロパティの変更が可能
+ */
+// var val1 = "var変数";
+// val1 = "var変数は上書き可能";
+// var val1 = "var変数は際宣言可能";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数";
+// val2 = "let変数は上書き可能";
+// console.log(val2);
+// const val3 = "const変数";
+// console.log(val3);
+
+// const val4 = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+// val4.name = "jak";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
